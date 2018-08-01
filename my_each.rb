9651fca64d4dfc9369722971342fdb91d23136c2
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-08-01 18:35:17 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arr) 
+  i = 0 
+  while arr.length != 0  
+    yield arr[i]
+  end
+    
+  return arr
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
